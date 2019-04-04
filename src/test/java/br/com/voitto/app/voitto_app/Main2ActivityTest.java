5ba2ffe4435a332db7554d8a30851e8a584be78b
Yan Esteves <yanagassi@outlook.com>
2019-04-04 14:18:41 -0300

    Add files via upload

---
+++ src/test/java/br/com/voitto/app/voitto_app/Main2ActivityTest.java
@@ -0,0 +1,12 @@
+package br.com.voitto.app.voitto_app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Main2ActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
